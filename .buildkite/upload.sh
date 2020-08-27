879df075849103157d140e6c1ba5a979e0174e18
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-08-27 16:12:11 -0300

    fix: upload to bucket
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ .buildkite/upload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+version="$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)"
+gsutil -m cp "presto-server/target/presto-server-$version.tar.gz" "gs://labs-dataproc/lib/presto-server/$version/presto-server-$version.tar.gz"
+gsutil -m cp "presto-cli/target/presto-cli-$version-executable.jar" "gs://labs-dataproc/lib/presto-cli/$version/presto-cli-$version-executable.jar"
