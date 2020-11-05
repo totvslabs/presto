152850d663cf24893d03d544e0b60858dc51d71b
Luiz Muller <contact@luizm.dev>
2020-11-05 09:45:34 -0300

    fix: cr

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,11 +2,11 @@
 set -e
 image="totvslabs/presto"
 
-# echo "~~~ build"
+echo "~~~ build"
 mvn clean install -DskipTests -T1C -ntp --builder smart
 version="$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)"
 
-# echo "~~~ upload to gcs"
+echo "~~~ upload to gcs"
 gsutil -m cp "presto-server/target/presto-server-$version.tar.gz" "gs://labs-dataproc-1/lib/presto-server/$version/presto-server-$version.tar.gz"
 gsutil -m cp "presto-cli/target/presto-cli-$version-executable.jar" "gs://labs-dataproc-1/lib/presto-cli/$version/presto-cli-$version-executable.jar"
 
